84d098135675acd1d7745a0146b55738f54b8d3d
priteshzhao <priteshzhao@gmail.com>
2021-07-05 17:35:39 -0700

    update start.sh

--- Start.sh
+++ Start.sh
@@ -1,5 +1,6 @@
-osascript -e 'tell application "Terminal" to activate' \
-  -e 'tell application "Terminal" to do script "npm run listenPending"' \
-  -e 'tell application "Terminal" to do script "python3 manage.py runserver 127.0.0.1:8000"'
-open "http://localhost:8000"
+cur_path=$(pwd)
+echo $cur_path
+osascript -e 'tell application "Terminal" to do script "cd /Users/jasonludwig/Downloads/front_run/Front-run-bot-MAC; npm run listenPending"'
+osascript -e 'tell application "Terminal" to do script "cd /Users/jasonludwig/Downloads/front_run/Front-run-bot-MAC; python3 manage.py runserver 127.0.0.1:8000"'
+osascript -e 'tell application "Terminal" to do script "open \"http://localhost:8000\""'
 
